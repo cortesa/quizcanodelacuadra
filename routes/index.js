b4aaee05d531f4ca66818a5dbbc1a306d5b8ea7f
canodelacuadra <canodelacuadra@gmail.com>
2015-06-02 11:14:07 +0200

    pagina autor

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+/* GET autor. */
+router.get('/autor', function(req, res) {
+  res.render('autor', { title: 'Creditos' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
